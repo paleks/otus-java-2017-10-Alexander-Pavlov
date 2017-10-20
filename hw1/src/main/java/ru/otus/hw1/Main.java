4a9d90363c87b3da524f65239e859f9e7c5b07e2
paleks <ksevalmslu@yandex.ru>
2017-10-20 04:07:29 +0400

    obfuscation added

--- hw1/src/main/java/ru/otus/hw1/Main.java
+++ hw1/src/main/java/ru/otus/hw1/Main.java
@@ -9,6 +9,10 @@ public class Main {
     public static void main(String[] strings) {
         List<String> list = Arrays.asList("a", "b", "C", "D", "E", "f");
 
+        new Main().print(list);
+    }
+
+    private void print(List<String> list) {
         Lists.reverse(list).forEach(System.out::println);
     }
 }
